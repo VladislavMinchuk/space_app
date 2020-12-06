724956292020db7afb8a3c7089291085fb050482
VladislavMinchuk <supervladislavgun@gmail.com>
2020-12-06 14:12:30 +0200

    Crareate validation of Astronaut model. Add CREATE Astronaut endpoint

--- app/controllers/api/astronauts_controller.rb
+++ app/controllers/api/astronauts_controller.rb
@@ -3,5 +3,23 @@ module Api
     def index
       render json: Astronaut.order('created_at DESC')
     end
+
+    def show
+      render json: Astronaut.find(params[:id])
+    end
+
+    def create
+      astronaut = Astronaut.create(astronaut_params)
+      if astronaut.errors.any?
+        return render json: { errors: astronaut.errors }
+      end
+      render json: astronaut
+    end
+
+    private
+
+      def astronaut_params
+        params.require(:astronaut).permit(:first_name, :last_name, :birthday, :country_id)
+      end
   end
 end
--- app/models/astronaut.rb
+++ app/models/astronaut.rb
@@ -1,3 +1,5 @@
 class Astronaut < ApplicationRecord
+  validates :first_name, :last_name, :birthday, :country_id, presence: true
+
   belongs_to :country
 end
