ddaa56657c879c49b62cbd38125ae669c90dc729
vladislav.minchuk <vladislav.minchuk@p2h.com>
2021-10-12 20:51:49 +0300

    Create 'destroy' method for astronaut controller

--- app/controllers/api/v1/astronauts_controller.rb
+++ app/controllers/api/v1/astronauts_controller.rb
@@ -7,6 +7,31 @@ module Api
         
         render json: @astronauts
       end
+
+      def create
+        @astronaut = Astronaut.new(astronaut_params)
+
+        if @astronaut.save
+          render json: @astronaut, status: :created
+        else
+          render json: @user.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        astronaut_id = params[:id]
+        @astronaut = Astronaut.find_by id: astronaut_id
+
+        if @astronaut
+          @astronaut.destroy
+        else
+          render json: { astronaut: "Astronaut with id='#{astronaut_id}' not found" }, status: :not_found
+        end
+      end
+
+      def astronaut_params
+        params.permit(:name, :country_id)
+      end
     end
   end
 end
